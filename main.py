4eed6722aaefd201bf72ddaf3b39a2db31871328
sdupourque <sdupourque@irap.omp.eu>
2021-05-06 10:15:23 +0200

    added several parameters

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
-import yaml
 from turb.analysis import Analysis
-from turb.project_model import BetaModel
 from astropy.table import Table
+import sys, os
 
-config = {'ps_region_size': '2r500',
-          'profile_model': BetaModel()}
+sys.path.append(os.getcwd())
+
+config_test= {'ps_region_size': '2r500',
+          'profile_model': 'BetaModel',
+          'ps_samples' : 2,
+          }
+
+config_XCOP_Beta = {'ps_region_size': '2r500',
+          'profile_model': 'BetaModel',
+          'ps_samples' : 100,
+          }
+
+config_XCOP_Median = {'ps_region_size': '2r500',
+          'profile_model': None,
+          'ps_samples' : 100,
+          }
 
 master_table = Table.read('data/master_table.fits')
-analysis = Analysis(master_table[17:19], config)
+test_table = master_table[7:9]
+xcop_table = master_table[master_table['SAMPLE'] == 'XCOP']
 
-analysis.launch()
+if __name__ == '__main__':
+
+    #analysis = Analysis(xcop_table, config_XCOP_Median, 'XCOP_Median')
+    #analysis.launch()
+
+    #print('Launching Beta analysis')
+
+    analysis = Analysis(test_table, config_test, 'test')
+    analysis.launch()
