9a149d7681f72f43af09e9766a96d399ef4a9ccb
sdupourque <sdupourque@irap.omp.eu>
2021-05-06 10:14:26 +0200

    simulation with random field inversion

--- simu/random_field.py
+++ simu/random_field.py
@@ -3,15 +3,37 @@ import numpy as np
 def pl_cutoff_PowerSpectrum(k):
 
     alpha = 11/3
-    norm = 1
+    norm = 1e-8
     k_inje = 1e-4
     k_piv = 1e-3
     k_disp = 1e-1
+    k = np.abs(k)
 
     return 10**norm*np.exp(-(k_inje/k)**2)*np.exp(-(k/k_disp)**2)*(k/k_piv)**(-alpha)
 
 class Fluctuation:
 
-    def __init__(self, P3D):
+    def __init__(self, P2D):
 
-        self.ps3d = P3D
+        self.P2D = P2D
+
+    def gen_complex_field(self,k):
+
+        mods = np.sqrt(np.random.gamma(3, scale=self.P2D(k)/3))
+        phase = np.random.uniform(low=0, high=2*np.pi, size=mods.shape)
+        mods = np.nan_to_num(mods)
+
+        return mods*(np.cos(phase) + 1j*np.sin(phase))
+
+    def gen_real_field(self, pixsize, shape):
+
+        kx = np.fft.fftfreq(max(shape), d=pixsize)
+        ky = np.fft.fftfreq(max(shape), d=pixsize)
+
+        Kx, Ky = np.meshgrid(kx, ky)
+        k = np.sqrt(Kx**2 + Ky**2)
+
+        complex_field = self.gen_complex_field(k)
+        real_field = np.fft.fftn(complex_field)
+
+        return real_field
--- simulated_cluster.py
+++ simulated_cluster.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import astropy.units as u
+from turb.extract_ps import Extractor
+from astropy.io import fits
 from simu.random_field import Fluctuation, pl_cutoff_PowerSpectrum
+from astropy.cosmology import FlatLambdaCDM
 
 k = np.geomspace(1e-6, 1e1, 1000)
 
@@ -10,3 +14,21 @@ plt.xlabel('k [kpc-1]')
 plt.ylabel('P3D')
 plt.loglog()
 plt.show()
+
+#%%
+cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+outmod = fits.getdata('analysis_results/test/clusters/A644/outmod_A644.fits')
+z = 0.07
+dr = 0.000694444444444445*cosmo.kpc_proper_per_arcmin(z).to(u.kpc/u.deg).value
+
+f = Fluctuation(pl_cutoff_PowerSpectrum)
+
+fig, ax = plt.subplots(ncols=3, nrows=3, constrained_layout=True, figsize=(10, 10))
+
+for i in range(9):
+
+    map = ax[i // 3, i % 3].imshow(np.real(f.gen_real_field(dr, outmod.shape)))
+
+plt.colorbar(mappable=map, ax=ax)
+plt.show()
+
